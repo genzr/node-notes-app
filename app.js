e37267bafbd69c36458b35b59e5446bb3f067ca8
Gennaro <genzr05@gmail.com>
2019-07-28 16:30:16 +1000

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const getNotes = require ('./notes')
+const chalk = require ('chalk')
+
+const command = process.argv[2]
+
+if (command === 'add') {
+    console.log('Adding note!')
+} else if (command === 'remove') {
+    console.log('removing note')
+}
+
---
+++ utils.js
@@ -0,0 +1,9 @@
+console.log('utils.js')
+
+const name = 'Trina'
+
+const add = function (a, b) {
+    return a + b
+}
+
+module.exports = add
