c61d6eedca4283139b49004fa32921bbcd9037c8
Gennaro <genzr05@gmail.com>
2019-07-29 21:38:51 +1000

    continued work on add note functionality

--- notes.js
+++ notes.js
@@ -1,9 +1,32 @@
+const fs = require('fs')
+
 const getNotes = function () {
     return 'Your notes....'
 }
 
-const addNote = function (title, body) {
+const loadNotes = function () {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json')
+        const notesJSON = dataBuffer.toString()
+        const notes = JSON.parse(notesJSON)
+        return notes
+    } catch (e) {
+        return []
+    }
+}
 
+const addNote = function (title, body) {
+    const notes = loadNotes()
+    notes.push({
+        title: title,
+        body: body
+    })
+    saveNotes(notes)
+}
+
+const saveNotes = function (notes) {
+    const notesJSON = JSON.stringify(notes)
+    fs.writeFileSync('notes.json', notesJSON)
 }
 
 module.exports = { 
